9feaa719dea7aca9d1bf0fc81cac3f004c200362
ashish-bazad <ashish.student.cse22@itbhu.ac.in>
2023-01-15 17:24:11 +0530

    added image

--- utils/contributors.js
+++ utils/contributors.js
@@ -62,7 +62,7 @@ const CONTRIBUTERS = [
     {
         handle_name: 'ashish-bazad',
         name: "Ashish",
-        image: "/images/eshaanagg.jpg",
+        image: "/images/AshishOP.jpeg",
         description: `
             <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
             <div class="text-center">
